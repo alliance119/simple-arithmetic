16a8111fcdb9fe482f035041d773cf3ccd9c3724
Vitalii Ielakov <vitalii.ielakov@gmail.com>
2016-04-25 21:23:51 +0300

    Add first simple-arithmetic.js with randomNumber and mathExpr functions

---
+++ simple-arithmetic.js
@@ -0,0 +1,53 @@
+/**
+ * Created by maryna-yelakova on 23.04.16.
+ */
+//Функция, которая генерирует случайные числа в заданном диапазоне
+function randomNumber(rangeBeggin,rangeEnd){
+    return Math.floor(Math.random()*(rangeEnd-rangeBeggin + 1)+rangeBeggin);
+}
+
+//Функция, котрая возвращает 1 пример в виде массива
+function mathExpr(maxNumber){
+    var slog1
+        , slog2
+        , sign;
+        slog1 = randomNumber(1, maxNumber);
+        slog2 = randomNumber(1, maxNumber);
+        sign = randomNumber(-2, 10);
+        if (sign > 0 && slog1 + slog2 <= maxNumber) {
+            return [slog1,1,slog2];
+            //  console.log(slog1 + " + " + slog2);
+        } else {
+
+                if (slog1 > slog2) {
+                    return [slog1,-1,slog2];
+              //      console.log(slog1 + " - " + slog2);
+                } else {
+                   return [slog2,-1,slog1];
+                  //  console.log(slog2 + " - " + slog1);
+                }
+            }
+    }
+console.log(mathExpr(20));
+
+//Функция, которая генрирует 10 примеров в виде двумерного массива
+function moreExpr(maxNumber){
+    for (var i=1;i<=10;i++) {
+       var expr = mathExpr(maxNumber);
+        console.log(expr);
+    }
+}
+moreExpr(20);
+
+var multiExpr = [];
+function testUnic(maxNumber){
+
+for(var i=1;i<=10;i++){
+    if (moreExpr(maxNumber) !== multiExpr[i]) {
+        moreExpr(maxNumber) = multiExpr[i];
+}
+    return multiExpr;
+}
+
+}
+console.log(testUnic(20))
